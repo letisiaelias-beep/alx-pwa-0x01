abc406d3490967375197262048f1b06844e1ded5
letisiaelias-beep <letisiaelias@gmail.com>
2025-10-26 16:31:12 +0300

    Create a Web App Manifest

--- alx-movie-app/next.config.js
+++ alx-movie-app/next.config.js
@@ -1,18 +1,39 @@
-// next.config.js
-const withPWAInit = require("@ducanh2912/next-pwa");
+// next.config.js (CommonJS, safe for Node + Next 16)
+const maybePWA = require('@ducanh2912/next-pwa');
+const nextPWA = (maybePWA && maybePWA.default) ? maybePWA.default : maybePWA;
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const baseConfig = {
   reactStrictMode: true,
+
+  // Use remotePatterns instead of deprecated images.domains
   images: {
-    domains: ["m.media-amazon.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/**',
+      },
+    ],
   },
+
+  // Add an empty turbopack config to avoid the "webpack config + no turbopack config" error
+  turbopack: {},
+
+  // If you need to configure other Next options, add them here
 };
 
-const withPWA = withPWAInit({
-  dest: "public",
-});
+let finalConfig = baseConfig;
 
-module.exports = withPWA({
-  ...nextConfig,
-});
+// If the PWA plugin is available (and is a function), use it
+if (typeof nextPWA === 'function') {
+  const withPWA = nextPWA({
+    dest: 'public',
+  });
+  finalConfig = withPWA(baseConfig);
+} else {
+  // eslint-disable-next-line no-console
+  console.warn('Warning: @ducanh2912/next-pwa not found or not a function. Continuing without PWA plugin.');
+}
+
+module.exports = finalConfig;
